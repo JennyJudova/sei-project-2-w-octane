a390744ce8841e5fce7dc2b1d891e93926ec0396
JennyJudova <jjudova@gmail.com>
2020-02-24 22:53:32 +0000

    filter works

--- app/helpers/filter-articles.js
+++ app/helpers/filter-articles.js
@@ -3,7 +3,7 @@ import { helper } from "@ember/component/helper";
 export function filterArticles(params /*, hash*/) {
   const [articles] = params;
   console.log(articles);
-  const art = articles.filter((artice, index) => index < 5);
+  const art = articles.filter((article, index) => index < 5);
   console.log(art);
   return art;
 }
